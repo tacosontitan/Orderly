95a13840f6e0ddb83a1cc2ab5ec75a71415a8a70
Hazel <jkdavis1691@gmail.com>
2025-12-07 20:41:56 -0600

    Rename mapping interface to mapper 😴

--- src/backend/Orderly.Core/IMapper.cs
+++ src/backend/Orderly.Core/IMapper.cs
@@ -1,11 +1,11 @@
 namespace Orderly.Core;
 
 /// <summary>
-/// Defines a mapping from one type to another.
+/// Defines a mapper for mapping one type to another.
 /// </summary>
 /// <typeparam name="TIn">Specifies the input type.</typeparam>
 /// <typeparam name="TOut">Specifies the output type.</typeparam>
-public interface IMapping<in TIn, out TOut>
+public interface IMapper<in TIn, out TOut>
 {
 	/// <summary>
 	/// Maps an instance of <typeparamref name="TIn"/> to an instance of <typeparamref name="TOut"/>.
