2165845d412d4d7f9fbed745febb2b86e733329a
Hazel <jkdavis1691@gmail.com>
2025-12-08 08:25:37 -0600

    Update documentation, add cancellation token

--- src/backend/Orderly.Risk.Contracts/IRiskService.cs
+++ src/backend/Orderly.Risk.Contracts/IRiskService.cs
@@ -5,5 +5,11 @@ namespace Orderly.Risk.Contracts;
 /// </summary>
 public interface IRiskService
 {
-	Task<RiskResponse> EvaluateRisk(RiskRequest request);
+	/// <summary>
+	/// Evaluates the risk of a given request.
+	/// </summary>
+	/// <param name="request">The risk evaluation request details.</param>
+	/// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+	/// <returns>The risk evaluation response.</returns>
+	Task<RiskResponse> EvaluateRisk(RiskRequest request, CancellationToken cancellationToken);
 }
